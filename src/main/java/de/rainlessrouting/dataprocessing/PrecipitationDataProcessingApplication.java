348c1e825fbda9ded2bfdaa3827db2b3b351de97
Philipp Kisters <philkisters@gmail.com>
2019-11-20 17:45:42 +0100

    removed old comments + using logger

--- src/main/java/de/rainlessrouting/dataprocessing/PrecipitationDataProcessingApplication.java
+++ src/main/java/de/rainlessrouting/dataprocessing/PrecipitationDataProcessingApplication.java
@@ -1,6 +1,6 @@
 package de.rainlessrouting.dataprocessing;
 
-import java.nio.file.Paths;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,16 +10,14 @@ import de.rainlessrouting.importer.*;
 
 @SpringBootApplication
 public class PrecipitationDataProcessingApplication {
+	private static final org.slf4j.Logger log = LoggerFactory.getLogger(PrecipitationDataProcessingApplication.class);
 
-	private static final String ARG_CLEAR_AUTO_MIN = "autoclear"; //=5 => 5 Minutes
+	private static final String ARG_CLEAR_AUTO_MIN = "autoclear"; //= Number of Minutes
 	private static final String ARG_CLEAR_DB = "cleardb";	
 	private static final String ARG_CLEAR_FILES = "clearfiles";
 
 	public static void main(String[] args) throws Exception {
 		ConfigurableApplicationContext context = SpringApplication.run(PrecipitationDataProcessingApplication.class, args);
-		
-		//new LoadingDualCorrectedTornado().updateDatabaseWithStaticPrecipitation();
-		//new ImportTestData().importData("src/main/resources/dual_corrected_tornado.nc", "DualCorrectedTornado");
 		
 		boolean clearDB = false;
 		boolean clearFiles = false;
@@ -41,19 +39,20 @@ public class PrecipitationDataProcessingApplication {
 			}
 			else
 			{
-				System.err.println("Unknown startup parameter: " + args[i]);
+				log.error("Unknown startup parameter: " + args[i]);
 			}
 		}
-		System.out.println("Startup with clearDB=" + clearDB + " clearFiles=" + true + " clearAutoMinutes=" + clearAutoMinutes);
-		
+		log.debug("Startup with clearDB=" + clearDB + " clearFiles=" + true + " clearAutoMinutes=" + clearAutoMinutes);
+
+		/* Import data from a live source. Use ftp.properties file */
 		//new ImportLive("ftp.properties", clearDB, clearFiles);
 
+		/* Import artificially created data. */
 		new ImportArtificial(clearDB);
 
-		String path = Paths.get(System.getProperty("user.dir"), "data_test", "Unwetter-2019-06-12").toString();
-		System.out.println("Path: " + path);
-
-		new ImportReplay(path, 1560343702000L, 1560348657000L, 30, true);
+		/* Import historical data from a folder. */
+		// String path = "src/main/resources";
+		// new ImportReplay(path, 1560343702000L, 1560348657000L, 30, true);
 		
 		if (clearAutoMinutes > 0)
 			new AutoClear(clearAutoMinutes);
